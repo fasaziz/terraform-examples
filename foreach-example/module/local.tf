216397fa24de68ae40ce191cf9437bc8a0f46daf
fasaziz <98742029+fasaziz@users.noreply.github.com>
2024-07-29 13:11:53 +0100

    Delete foreach-example directory

--- foreach-example/module/local.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  circuit_names = { for circuit in var.circuit_config : circuit.circuit_name => circuit }
-  peering_type = { for peer in var.peering_config : peer.peering_type => peer }
-}
